376392a0d2449c009d9f77dd7f673293b618bde8
NrdyBhu1 <NrdyBhu1@gmail.com>
2022-09-19 20:37:12 +0530

    [pog -> pog2D]

--- setup.py
+++ setup.py
@@ -3,22 +3,25 @@ from setuptools import setup, Extension
 
 pog = Extension('pog',
 	define_macros = [
+		# -D_REENTRANT
 		("_REENTRANT", None)
 	],
 	sources = [
 		'src/pog.c',
 	],
 	include_dirs = [
+		# pkg-config --cflags sdl2
 		'/nix/store/bgc3rvghyjdby7lw94hkjjgwv1d20zp6-SDL2-2.0.20-dev/include/SDL2',
 	],
 	extra_link_args = [
+		# pkg-config --libs sdl2
 		'-L/nix/store/536n1g1ca44v2q3c21809wz8ff602lmr-SDL2-2.0.20/lib',
 		'-Wl,-rpath,/nix/store/536n1g1ca44v2q3c21809wz8ff602lmr-SDL2-2.0.20/lib',
 		'-Wl,--enable-new-dtags',
 		'-lSDL2'
 	])
 
-setup(name = 'pog',
-	version = '0.0.6-dev',
+setup(name = 'pog2D',
+	version = 'dev-0.0.6',
 	description = 'A simple 2D library',
 	ext_modules = [pog])
