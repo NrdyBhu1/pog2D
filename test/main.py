5473f131639f57f03752e195ae58273809a867f4
NrdyBhu1 <NrdyBhu1@gmail.com>
2022-09-20 20:22:29 +0530

    add event handlers

--- test/main.py
+++ test/main.py
@@ -1,6 +1,14 @@
 import pog
 
+def pre_loop():
+	print("This is called before the loop")
+
+def post_loop():
+	print("This is called after the loop")
+
 pog.init()
 c = pog.Context(title="Hello, World!", width=800, height=600)
+c.add_handler("preloop", pre_loop)
+c.add_handler("postloop", post_loop)
 c.run()
 pog.quit()
